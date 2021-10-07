205c9625aa1ea63888f6dce40fb662fc597fa1d2
pipeCL <pipx2012@gmail.com>
2021-10-07 10:20:36 -0300

    primer commit

---
+++ app/Http/Controllers/API/PacienteController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\ActualizarPacienteRequest;
+use App\Http\Requests\GuardarPacienteRequest;
+use App\Models\Paciente;
+use Illuminate\Http\Request;
+
+class PacienteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Paciente::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(GuardarPacienteRequest $request)
+    {
+        Paciente::create($request->all());
+        return response()->json([
+                'res' => true,
+                'msg' => 'Paciente Guardado Correctamente'
+            ]
+            , 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Paciente $paciente)
+    {
+        return response()->json([
+            'res' => true,
+            'paciente' => $paciente
+        ], 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(ActualizarPacienteRequest $request, Paciente $paciente)
+    {
+        $paciente->update($request->all());
+        return response()->json([
+            'res' => true,
+            'mensaje' => 'Paciente Actualizado Correctamente'
+        ], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Paciente $paciente)
+    {
+        $paciente->delete();
+
+        return response()->json([
+            'res' => true,
+            'mensaje' => 'Paciente Eliminado Correctamente'
+        ], 200);
+    }
+}
